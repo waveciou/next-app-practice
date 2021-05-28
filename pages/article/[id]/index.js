42ddd9d03de0367c9e25519af4c1e66f865e42c1
丘惟夫 <waveciou@gmail.com>
2021-05-28 22:04:46 +0800

    getServerSideProps practice

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
-const Article = () => {
+const Article = ({ article }) => {
   const router = useRouter();
   // router.query.id 可以取得巢狀路由的參數（網址上的子路由名稱）
   const { id } = router.query;
@@ -9,15 +10,26 @@ const Article = () => {
   return (
     <div className="wrap">
       <Head>
-        <title>{ `Article ${id}` }</title>
+        <title>{ `${article.title}` }</title>
       </Head>
 
-      <h1>{ `Article Page ${id}` }</h1>
+      <h1>{ `${article.title}` }</h1>
       <div className="content">
-        <p>This is Article Page</p>
+        <p>{ `${article.body}` }</p>
       </div>
     </div>
   )
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+  const article = await res.data;
+
+  return {
+    props: {
+      article
+    }
+  }
 };
 
 export default Article;
