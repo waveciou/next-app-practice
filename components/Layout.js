e4b6c173a3470eab1e69810dbee7ae8b2498ebaa
丘惟夫 <waveciou@gmail.com>
2021-05-29 12:24:18 +0800

    add meta component and favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,9 @@
 import styles from '../styles/modules/Layout.module.scss';
 import Menu from './Menu';
+import Meta from './Meta';
 import { useEffect } from 'react';
+
+// Head Meta 若要模組化共用，可以放在 Layout 這一層
 
 const Layout = ({ children }) => {
   useEffect(() => {
@@ -8,7 +11,8 @@ const Layout = ({ children }) => {
   }, []);
 
   return (
-    <div>
+    <div className="__layout">
+      <Meta />
       <Menu />
       <main className={styles.container}>
         { children }
